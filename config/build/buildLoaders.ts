580a53fe24b1fa556bd3ae730d7ca7f974656543
Ellie <diana.lyutaya12@gmail.com>
2025-06-06 22:29:50 +0300

    refactor pdf for build

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -74,7 +74,7 @@ export function buildLoaders(options: BuildOptions): ModuleOptions['rules'] {
             options: {
               name: '[name].[ext]',
               outputPath: 'static/pdf/',
-              publicPath: '/assets/pdf/',
+              publicPath: '/static/pdf/',
             },
           },
         ],
